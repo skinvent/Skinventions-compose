300631c16568a9a998f52c968aaffe2a0faab97a
skinvent <sketcherwaree@gmail.com>
2024-10-05 07:59:16 +0100

    Feat: Creating the App

---
+++ README.md
@@ -0,0 +1,11 @@
+![Logo](banner.png)
+
+Skinventions' Compose Learning Sample
+
+
+
+
+## Documentation
+Skinventions is started Learning Jetpack Compose so this a sample app to help him More learning Jetpack 
+By the way if the app finished or not finished releasing you can use this source code 
+
---
+++ app/src/main/java/com/skinvent/learningcomposable/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.skinvent.learningcomposable
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.material.Text
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
+
+import com.skinvent.learningcomposable.ui.theme.MyComposeApplicationTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyComposeApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background) {
+                    Grids()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
+    Text(text = "Skinvent Compose", modifier = modifier)
+}
+
+@Preview(showBackground = true)
+
+@Composable 
+fun Grids (){
+LazyVerticalGrid(
+    columns = GridCells.Adaptive(minSize = 128.dp)
+) {
+items(texts){
+Greeting()
+             }
+ }
+         }
